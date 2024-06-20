d7dcff682ce2d73c4a47368dd505018bbd92f05d
shivamdinde <shivamdinde60@gmail.com>
2024-06-20 22:05:52 +0530

    Worked with Sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -11,6 +12,7 @@ function App() {
         <div className="app">
           <main className="content">
             <Topbar />
+            <Sidebar />
           </main>
         </div>
         ;
