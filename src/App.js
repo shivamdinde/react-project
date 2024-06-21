e14699df1cb89ba6a09804af33392819039f70e6
shivamdinde <shivamdinde60@gmail.com>
2024-06-21 11:59:43 +0530

    Worked with Teams Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./scenes/dashboard/Dashboard";
+import Team from "./scenes/team/Team";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -10,9 +13,21 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar />
           <main className="content">
             <Topbar />
-            <Sidebar />
+
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              {/* <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} /> */}
+            </Routes>
           </main>
         </div>
         ;
