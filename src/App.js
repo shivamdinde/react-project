fd1b6bd5712c8c7fa5a3ab1bcf167315dae0cd99
shivamdinde <shivamdinde60@gmail.com>
2024-06-21 14:40:27 +0530

    Worked with Contacts Page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Sidebar from "./scenes/global/Sidebar";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./scenes/dashboard/Dashboard";
 import Team from "./scenes/team/Team";
+import Contacts from "./scenes/contacts/Contacts";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -20,8 +21,8 @@ function App() {
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
-              {/* <Route path="/contacts" element={<Contacts />} />
-              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/contacts" element={<Contacts />} />
+              {/* <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
