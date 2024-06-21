f54e6bb8ed62bf9e682e16a53ae4cd70b0b1dc8c
shivamdinde <shivamdinde60@gmail.com>
2024-06-21 17:22:41 +0530

    Worked with Form page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Dashboard from "./scenes/dashboard/Dashboard";
 import Team from "./scenes/team/Team";
 import Contacts from "./scenes/contacts/Contacts";
 import Invoices from "./scenes/invoices/Invoices";
+import Form from "./scenes/form/Form";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -24,8 +25,8 @@ function App() {
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
-              {/* <Route path="/form" element={<Form />} />
-              <Route path="/bar" element={<Bar />} />
+              <Route path="/form" element={<Form />} />
+              {/* <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
               <Route path="/faq" element={<FAQ />} /> */}
