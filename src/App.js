a71d435128cbe8351f6b4489df1ec42faa72f41d
shivamdinde <shivamdinde60@gmail.com>
2024-06-22 17:13:13 +0530

    Worked with FAQ page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Team from "./scenes/team/Team";
 import Contacts from "./scenes/contacts/Contacts";
 import Invoices from "./scenes/invoices/Invoices";
 import Form from "./scenes/form/Form";
+import FAQ from "./scenes/faq/FAQ";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -26,10 +27,10 @@ function App() {
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
+              <Route path="/faq" element={<FAQ />} />
               {/* <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
-              <Route path="/line" element={<Line />} />
-              <Route path="/faq" element={<FAQ />} /> */}
+              <Route path="/line" element={<Line />} /> */}
             </Routes>
           </main>
         </div>
