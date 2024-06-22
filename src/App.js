dd788a79761c539a73e14f9799c9d4b014609b87
shivamdinde <shivamdinde60@gmail.com>
2024-06-22 17:51:07 +0530

    Worked with Charts pages

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import Contacts from "./scenes/contacts/Contacts";
 import Invoices from "./scenes/invoices/Invoices";
 import Form from "./scenes/form/Form";
 import FAQ from "./scenes/faq/FAQ";
+import Bar from "./scenes/bar/Bar";
+import Pie from "./scenes/pie/Pie";
+import Line from "./scenes/line/Line";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -28,9 +31,9 @@ function App() {
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/faq" element={<FAQ />} />
-              {/* <Route path="/bar" element={<Bar />} />
+              <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
-              <Route path="/line" element={<Line />} /> */}
+              <Route path="/line" element={<Line />} />
             </Routes>
           </main>
         </div>
